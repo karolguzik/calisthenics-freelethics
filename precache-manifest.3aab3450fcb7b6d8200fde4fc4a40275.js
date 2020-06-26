363bbcba8b2290ba2e78ab5c0815189ece84961f
Karol <guzik912@o2.pl>
2020-06-26 16:08:26 +0200

    Updates

---
+++ precache-manifest.3aab3450fcb7b6d8200fde4fc4a40275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d543eca74f214fa9bde4cdc48e62faa",
+    "url": "/calisthenics-freelethics/index.html"
+  },
+  {
+    "revision": "511d093871ebd615700d",
+    "url": "/calisthenics-freelethics/static/js/2.0cfb0bc4.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/calisthenics-freelethics/static/js/2.0cfb0bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87421fed67039fe913cc",
+    "url": "/calisthenics-freelethics/static/js/main.64b09f1b.chunk.js"
+  },
+  {
+    "revision": "0dfb9d49f1d55fb43db4",
+    "url": "/calisthenics-freelethics/static/js/runtime-main.a44f0784.js"
+  },
+  {
+    "revision": "5a5f1b4711ed30cb14b3e59aeb4bc239",
+    "url": "/calisthenics-freelethics/static/media/bgc-1.5a5f1b47.png"
+  }
+]);
