2261d4af35fce6e56a62d5d42dd0b4c72e43ae7d
Karol <guzik912@o2.pl>
2020-07-23 19:28:27 +0200

    change mongoURI

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const trainingRoutes = require('./routes/trainings');
 const progressRoutes = require('./routes/progress');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -17,6 +18,14 @@ app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/trainings', trainingRoutes);
 app.use('/api/progress', progressRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('../build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
